c1ea16991480fa2f654c1c86e133b552bd7134e8
unknown <augkinyi@gmail.com>
2023-11-05 17:18:33 +0300

    Third commit

--- Main.js
+++ Main.js
@@ -5,3 +5,10 @@ loginbtn.addEventListener("click",() => {
     alert("you clicked login")
     console.log("login");
 })
+
+const GetQuoteNow = document.getElementById("btn1");
+
+GetQuoteNow.addEventListener("click", () => {
+  alert("You clicked Get Quote now");
+  console.log("btn1");
+});
